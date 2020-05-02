297ef780d8b23bfceab1fd8957b35814b682cc67
Rob Bishop <c.sagan.ritual@gmail.com>
2020-05-01 17:20:17 -0700

    Initial Commit

---
+++ Foil/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Foil
+//
+//  Created by Rob Bishop on 5/1/20.
+//  Copyright © 2020 Boring Software. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
